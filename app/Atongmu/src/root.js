62ebd252dadd3c4eaffe8a27b73deab3c5481aaa
Zhiyu Li <junxianalan@gmail.com>
2017-07-13 22:29:23 +0800

    添加app.js 和root.js

---
+++ app/Atongmu/src/root.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import React from 'react'
+import {
+	Navigator
+} from 'react-native';
+import { Provider } from 'react-redux'
+import configureStore from './store/store.js'
+import App from './containers/app.js'
+const store = configureStore();
+
+class Root extends React.Component {
+	render() {
+		return (
+			<Provider store={ store }>
+				<Navigator
+				    initialRoute={{ component: App }}
+				    configureScene={(route, routeStack) => {
+				        return Navigator.SceneConfigs.FloatFromRight;
+				    }}
+				    renderScene={(route, navigator) => {
+				    	let Component = route.component;
+				        return <Component {...route.params} navigator={navigator} />
+				        //  上面的route.params 是为了方便后续界面间传递参数用的
+				    }} />
+			</Provider>
+		);
+	}
+}
+export default Root;
+//https://github.com/yongqianvip/RN-ListViewLoadMore
