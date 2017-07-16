8e7bcc08bae177d77d7a91d933926af95f49d2c4
Zhiyu Li <junxianalan@gmail.com>
2017-07-16 23:16:09 +0800

    正常运行主界面

---
+++ app/Atongmu/src/Containers/app.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+	View,
+  Text
+} from 'react-native';
+// import { connect } from 'react-redux'
+
+import HomeContainers from './HomeContainers'
+import OtherContainer from './OtherContainer'
+import TabNavigator from 'react-native-tab-navigator'
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Navigator} from 'react-native-deprecated-custom-components'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedTab: 'home'
+        };
+    }
+
+    render() {
+
+        return (
+            <TabNavigator tabBarStyle={{ backgroundColor:'white' }} style={{backgroundColor: 'white'}}>
+                <TabNavigator.Item
+                    title="Home"
+                    selected={this.state.selectedTab === 'home'}
+                    renderIcon={() => <Icon name={ 'ios-home' } size={30} color={'gray'}/>}
+                    renderSelectedIcon={() => <Icon name={ 'ios-home' } size={30} color={'#4E78E7'}/>}
+                    onPress={() => this.setState({ selectedTab: 'home' })}>
+                    <HomeContainers {...this.props} />
+                    
+                </TabNavigator.Item>
+
+                <TabNavigator.Item
+                    title="Other"
+                    selected={this.state.selectedTab === 'other'}
+                    renderIcon={() => <Icon name={ 'ios-more' } size={30} color={'gray'}/>}
+                    renderSelectedIcon={() => <Icon name={ 'ios-more' } size={30} color={'#4E78E7'}/>}
+                    onPress={() => this.setState({ selectedTab: 'other' })}>
+                    <OtherContainer {...this.props}/>
+                </TabNavigator.Item>
+            </TabNavigator>
+        );
+    }
+
+}
+
+export default App;
--- app/Atongmu/src/root.js
+++ app/Atongmu/src/root.js
@@ -1,29 +1,24 @@
 'use strict';
 
 import React from 'react'
-import {
-	Navigator
-} from 'react-native';
-import { Provider } from 'react-redux'
-import configureStore from './store/store.js'
-import App from './containers/app.js'
-const store = configureStore();
+
+// import { Provider } from 'react-redux'
+import {Navigator} from 'react-native-deprecated-custom-components'
+import App from './Containers/app.js'
 
 class Root extends React.Component {
 	render() {
 		return (
-			<Provider store={ store }>
-				<Navigator
-				    initialRoute={{ component: App }}
-				    configureScene={(route, routeStack) => {
-				        return Navigator.SceneConfigs.FloatFromRight;
-				    }}
-				    renderScene={(route, navigator) => {
-				    	let Component = route.component;
-				        return <Component {...route.params} navigator={navigator} />
-				        //  上面的route.params 是为了方便后续界面间传递参数用的
-				    }} />
-			</Provider>
+				// <Navigator
+				//     initialRoute={{ component: App }}
+				//     configureScene={(route, routeStack) => {
+				//         return Navigator.SceneConfigs.FloatFromRight;
+				//     }}
+				//     renderScene={(route, navigator) => {
+				//     	let Component = route.component;
+				//         return <Component {...route.params} navigator={navigator} />
+				//     }} />
+				<App/>
 		);
 	}
 }
