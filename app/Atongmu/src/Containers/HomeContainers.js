274a21d545b3dd7771f81b6a4fca1735eb92db0c
Lewin <lewin.li@ihr360.com>
2017-07-19 19:43:26 +0800

    添加Listview

--- app/Atongmu/src/Containers/HomeContainers.js
+++ app/Atongmu/src/Containers/HomeContainers.js
@@ -9,23 +9,27 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  ListView,
   View
 } from 'react-native';
 
 export default class HomeContainers extends Component {
+  getInitialState: function() {
+      var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        return {
+          dataSource: ds.cloneWithRows(['row1', 'row 2','row 3','row 4','row 5','row 6','row 7','row 8']),
+        };
+    },
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Home
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) =><Text>{rowData}</Text>}
+        />  
       </View>
     );
   }
