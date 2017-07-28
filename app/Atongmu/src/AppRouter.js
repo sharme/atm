1c9ba83bd565cab5bcb55d5b0907df94f99ec6c2
Lewin <lewin.li@ihr360.com>
2017-07-28 18:31:43 +0800

    test

--- app/Atongmu/src/AppRouter.js
+++ app/Atongmu/src/AppRouter.js
@@ -1,6 +1,14 @@
 'use strict';
 
-import React from 'react'
+// import React from 'react'
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
 
 // import { Provider } from 'react-redux'
 import {Navigator} from 'react-native-deprecated-custom-components'
@@ -30,43 +38,77 @@ import DesignList from './Containers/DesignList'
  * 界面对象 结束
  */
 
-// class Root extends React.Component {
-// 	render() {
-// 		return (
-// 				// <Navigator
-// 				//     initialRoute={{ component: App }}
-// 				//     configureScene={(route, routeStack) => {
-// 				//         return Navigator.SceneConfigs.FloatFromRight;
-// 				//     }}
-// 				//     renderScene={(route, navigator) => {
-// 				//     	let Component = route.component;
-// 				//         return <Component {...route.params} navigator={navigator} />
-// 				//     }} />
-// 				// <App/>
-// 		);
-// 	}
-// }
+ export default class AppRouter extends Component {
+   render() {
+     return (
+         <Router>
+           <Scene key="lightbox" lightbox leftButtonTextStyle={{color: 'green'}} backButtonTextStyle={{color: 'red'}}>
+             <Scene key="modal" modal hideNavBar>
+                 <Scene key="tabbar" gestureEnabled={false} tabs activeBackgroundColor='#ddd' initial>
+                       <Scene key="home" component={HomeContainers} title="主页" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
+                       <Scene key="exhibition" component={ExhibitionActivityList} title="展会" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
+                       <Scene key="design" component={DesignList} title="设计师" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
+                       <Scene key="me" component={AboutMe} title="我的" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
+                 </Scene>
+                 <Scene key="other" component={OtherContainer}/>
+             </Scene>
+           </Scene>
+         </Router>
+      //  <View style={styles.container}>
+      //    <Text style={styles.welcome}>
+      //      Other
+      //    </Text>
+      //    <Text style={styles.instructions}>
+      //      To get started, edit index.ios.js
+      //    </Text>
+      //    <Text style={styles.instructions}>
+      //      Press Cmd+R to reload,{'\n'}
+      //      Cmd+D or shake for dev menu
+      //    </Text>
+      //  </View>
+     );
+   }
+ }
+
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#F5FCFF',
+   },
+   welcome: {
+     fontSize: 20,
+     textAlign: 'center',
+     margin: 10,
+   },
+   instructions: {
+     textAlign: 'center',
+     color: '#333333',
+     marginBottom: 5,
+   },
+ });
 // export default Root;
-const AppRouter = () =>(
-  <Router>
-    <Scene key="lightbox" lightbox leftButtonTextStyle={{color: 'green'}} backButtonTextStyle={{color: 'red'}}>
-      <Scene key="modal" modal hideNavBar>
-          <Scene key="tabbar"
-             gestureEnabled={false}
-             tabs
-             activeBackgroundColor='#ddd'
-             initial
-            >
-                <Scene key="home" component={HomeContainers} title="主页" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
-                <Scene key="exhibition" component={ExhibitionActivityList} title="展会" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
-                <Scene key="design" component={DesignList} title="设计师" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
-                <Scene key="me" component={AboutMe} title="我的" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
-          </Scene>
-          <Scene key="other" component={OtherContainer}/>
-      </Scene>
-    </Scene>
-  </Router>
-);
-export default AppRouter;
+// const AppRouter = () =>(
+//   <Router>
+//     <Scene key="lightbox" lightbox leftButtonTextStyle={{color: 'green'}} backButtonTextStyle={{color: 'red'}}>
+//       <Scene key="modal" modal hideNavBar>
+//           <Scene key="tabbar"
+//              gestureEnabled={false}
+//              tabs
+//              activeBackgroundColor='#ddd'
+//              initial
+//             >
+//                 <Scene key="home" component={HomeContainers} title="主页" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
+//                 <Scene key="exhibition" component={ExhibitionActivityList} title="展会" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
+//                 <Scene key="design" component={DesignList} title="设计师" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
+//                 <Scene key="me" component={AboutMe} title="我的" icon={< Icon name = {'ios-home'} size = {30} color = {'gray'}/>}/>
+//           </Scene>
+//           <Scene key="other" component={OtherContainer}/>
+//       </Scene>
+//     </Scene>
+//   </Router>
+// );
+// export default AppRouter;
 
 //https://github.com/yongqianvip/RN-ListViewLoadMore
