abf29c2a20b822a32e5a5f5bb3c56676f76ad562
sharme <yaomiwang@qq.com>
2017-09-13 23:58:18 +0800

    atm welcome sites

--- atm_web/app.js
+++ atm_web/app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/templated-urban')));
 
 app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
